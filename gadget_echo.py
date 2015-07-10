74b47fe43acda82d85b247aa9c236e53c0323771
Beda Kosata <bedrich.kosata@nic.cz>
2015-07-10 17:24:50 +0200

    make input device adjustable from command line in the gadgets_echo demo

--- gadget_echo.py
+++ gadget_echo.py
@@ -5,14 +5,21 @@ Just run it from the command line and watch the messages printed
 on the standard output.
 """
 
+from __future__ import print_function
+import sys
 import datetime
 
 from device import Device
 
 if __name__ == "__main__":
-    device = Device(device="/dev/ttyUSB1")
+    if len(sys.argv) <= 1:
+        device_name = "/dev/ttyUSB0"
+    else:
+        device_name = sys.argv[1]
+    print("Using '{0}' as input device".format(device_name), file=sys.stderr)
+    device = Device(device=device_name)
     reader = device.gen_lines()
     while True:
         line = reader.next()
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print date, line
+        print(date, line)
