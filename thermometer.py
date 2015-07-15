68ce13c0e5a936f464e667e2e528959ae02dfb2d
Beda Kosata <bedrich.kosata@nic.cz>
2015-07-15 11:28:26 +0200

    remove message deduplication - not needed anymore

--- thermometer.py
+++ thermometer.py
@@ -29,26 +29,18 @@ if __name__ == "__main__":
     print("Using '{0}' as input device".format(device_name), file=sys.stderr)
     device = Device(device=device_name)
     reader = device.gen_lines()
-    # we use the following to detect duplicated messages
-    last_comm_time = time()
-    last_comm = None
     # we keep the output file open and flush it after each write
     with file("thermometer.csv", "w") as outfile:
         outfile.write("date,temperature\n")
         while True:
             line = reader.next()
             print(line)
-            if line != last_comm or time() - last_comm_time > 0.1:
-                # thermometer does not use message ID, so we use time of arrival
-                # to decide if a message is duplicate of a previous one
-                date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                m = re.search("^\[(\d{8})\].+INT:(....)", line)
-                if m:
-                    thermometer_id, temp = m.groups()
-                    outfile.write("{0},{1}\n".format(date, temp))
-                    print("{0}: {1}".format(date, temp), file=sys.stderr)
-                outfile.flush()
-                last_comm = line
-                last_comm_time = time()
+            date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            m = re.search("^\[(\d{8})\].+INT:(....)", line)
+            if m:
+                thermometer_id, temp = m.groups()
+                outfile.write("{0},{1}\n".format(date, temp))
+                print("{0}: {1}".format(date, temp), file=sys.stderr)
+            outfile.flush()
 
 
